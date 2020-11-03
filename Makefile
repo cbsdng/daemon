23a0be909cfaa72101f7ad193b79c75f7bc632aa
Goran Mekić <meka@tilda.center>
2020-11-03 17:57:32 +0100

    Update submodules

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
 SERVICE = daemon
 REGGAE_PATH := /usr/local/share/reggae
+DEVEL_MODE = YES
 
 .include <${REGGAE_PATH}/mk/service.mk>
--- shell/provision.sh
+++ shell/provision.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
 
-pkg install -y replxx
+pkg install -y ccache replxx
+cat << EOF >/etc/make.conf
+NO_CLEAN=YES
+WITH_CCACHE_BUILD=YES
+CCACHE_DIR=/usr/home/devel/.ccache
+SU_CMD=/usr/local/bin/sudo -E sh -c
+EOF
